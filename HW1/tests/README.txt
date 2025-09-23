24dd479281e4b3e2a70aba3350560e5e267168a4
nbeahm <nbeahms1@gmail.com>
2025-09-22 23:31:19 -0600

    HW1 Push2
    added readme, removed test7 as there was no test

---
+++ HW1/tests/README.txt
@@ -0,0 +1,2 @@
+To run these tests/code, use a python environment, or a terminal. If you are using a terminal enter "pytest [filename].py" and
+you should see if the test succeeds or fails after.
--- HW1/tests/test_task7.py
+++
@@ -1,9 +0,0 @@
-import pytest
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-from task1 import helloworld
-
-def helloworldTest():
-    assert helloworld() == "hello world!"
-helloworldTest()
